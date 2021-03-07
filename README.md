1d0b37cba7385f5590ad99e48f9387e3bdfa9d24
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-07 16:43:49 -0500

    Strip down to template

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Libra Cleaner
+# rust-sysmodule-template
 
-This is a simple sys-module to clean up files on first run that might cause crashes and are not needed anymore. It's also programmed in Rust, because why not :)
+This is a simple template for getting started with writing Nintendo Switch system modules with Rust.
 
-## Licensing
+## Building
 
-This software is licensed under the EUROPEAN UNION PUBLIC LICENCE version 1.2, with the following exemptions:
+To build, you'll need to install sprinkle:
 
-- [jam1garner](https://github.com/jam1garner) may use this project under any license
+```
+cargo install --git https://github.com/aarch64-switch-rs/sprinkle
+```
+
+then, within the template folder run the following command:
+
+```
+sprinkle nro
+```
 
 ## Credits:
 
+- tomGER for libra-cleaner (which was used as a base)
 - XorTroll for his Rust port and help: https://github.com/aarch64-switch-rs/nx
 - The original Kosmos Cleaner by Nichole Mattera: https://git.nicholemattera.com/NicholeMattera/Kosmos-Cleaner
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-/*
-* Copyright 2021 tomGER, git@tomger.eu
-*
-* Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
-* You may not use this work except in compliance with theLicence.
-*
-* You may obtain a copy of the Licence at: https://joinup.ec.europa.eu/software/page/eupl
-*
-* Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the Licence for the specific language governing permissions and limitations under the Licence.
-*/
-
 #![no_std]
 #![no_main]
 
 extern crate alloc;
-use alloc::string::String;
 
 extern crate nx;
 use nx::result::*;
@@ -26,8 +12,12 @@ use nx::fs;
 
 use core::panic;
 
+#[allow(non_upper_case_globals)]
+const KiB: usize = 0x400;
+const HEAP_SIZE: usize = 16 * KiB;
+
 // We're using 16KB of heap
-static mut STACK_HEAP: [u8; 0x00004000] = [0; 0x00004000];
+static mut STACK_HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 
 #[no_mangle]
 pub fn initialize_heap(_hbl_heap: util::PointerAndSize) -> util::PointerAndSize {
@@ -36,35 +26,12 @@ pub fn initialize_heap(_hbl_heap: util::PointerAndSize) -> util::PointerAndSize 
     }
 }
 
-pub fn exists_folder(path: &str) -> bool {
-    match fs::get_entry_type(String::from(path)) {
-        Ok(i) => match i { 
-            fs::DirectoryEntryType::File => false,
-            fs::DirectoryEntryType::Directory => true
-        }
-        Err(_) => false
-    }
-}
-
 #[no_mangle]
 pub fn main() -> Result<()> {
     fs::initialize()?;
     fs::mount_sd_card("sdmc")?;
 
-    let delete_folders = [
-    "sdmc:/atmosphere/contents/010000000007E51A/flags",
-    "sdmc:/atmosphere/contents/010000000007E51A",
-    "sdmc:/atmosphere/exefs_patches/nichole_logo",
-    "sdmc:/atmosphere/contents/010000000000DA7A/flags",
-    "sdmc:/atmosphere/contents/010000000000DA7A",
-    "sdmc:/rustisbad"
-    ];
-
-    for i in 0..delete_folders.len() {
-        if exists_folder(delete_folders[i]) {
-            fs::delete_directory(String::from(delete_folders[i]))?;
-        }
-    }
+    // do stuff here
 
     fs::finalize();
     Ok(())
