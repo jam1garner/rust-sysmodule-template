9f1118258ec9a561cc15bf07e2b383c32c734fbb
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-07 18:09:41 -0500

    Move examples to use macros

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
-extern crate nx;
-use nx::result::*;
-use nx::util;
-use nx::diag::assert;
-use nx::diag::log;
-use nx::fs;
+use nx::{
+    diag::{assert, log},
+    result::*,
+    util,
+    fs
+};
 
 use core::panic;
 
 #[allow(non_upper_case_globals)]
 const KiB: usize = 0x400;
-const HEAP_SIZE: usize = 16 * KiB;
 
 // We're using 16KB of heap
+const HEAP_SIZE: usize = 16 * KiB;
+
+#[nx::heap]
 static mut STACK_HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 
-#[no_mangle]
-pub fn initialize_heap(_hbl_heap: util::PointerAndSize) -> util::PointerAndSize {
-    unsafe {
-        util::PointerAndSize::new(STACK_HEAP.as_mut_ptr(), STACK_HEAP.len())
-    }
-}
-
-#[no_mangle]
+#[nx::main]
 pub fn main() -> Result<()> {
     fs::initialize()?;
     fs::mount_sd_card("sdmc")?;
